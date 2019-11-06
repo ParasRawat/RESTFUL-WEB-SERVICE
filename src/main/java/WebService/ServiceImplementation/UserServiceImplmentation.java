ca86f10b4643792c6e529a353cdaba6cf6b5c8f4
Paras Rawat <parasmyname@gmail.com>
2019-11-06 10:39:39 +0530

    GENERATING SPRING JWT FOR USER AUTHENTICATION

--- src/main/java/WebService/ServiceImplementation/UserServiceImplmentation.java
+++ src/main/java/WebService/ServiceImplementation/UserServiceImplmentation.java
@@ -7,10 +7,14 @@ import WebService.Shared.dto.UserDto;
 import WebService.Shared.dto.Utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImplmentation implements UserService {
@@ -45,7 +49,11 @@ public class UserServiceImplmentation implements UserService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+      UserEntity entity=userRepository.findByEmail(email);
+      if(entity==null) throw new UsernameNotFoundException(email);
+
+      return new User(entity.getEmail(),entity.getEncryptedPassword(),new ArrayList<>());
+
     }
 }
