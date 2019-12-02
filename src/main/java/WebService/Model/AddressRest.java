80b43dcb180071bde9891c3e3c7bf993cecc865a
Paras Rawat <parasmyname@gmail.com>
2019-12-02 09:05:01 +0530

    Adding hateos support in the project

--- src/main/java/WebService/Model/AddressRest.java
+++ src/main/java/WebService/Model/AddressRest.java
@@ -1,6 +1,8 @@
 package WebService.Model;
 
-public class AddressRest {
+import org.springframework.hateoas.ResourceSupport;
+
+public class AddressRest extends ResourceSupport {
 
     private String addressId;
     private String city;
