443f5dd83595bfb86e992386afbe57792b4b34c2
Paras Rawat <parasmyname@gmail.com>
2020-02-25 00:35:04 +0530

    Deeper understanding of Junit 5, mockito, creating required mocks for create user and testing create user

--- src/test/java/WebService/UserServiceImplTest.java
+++ src/test/java/WebService/UserServiceImplTest.java
@@ -3,16 +3,22 @@ package WebService;
 import WebService.Entity.UserEntity;
 import WebService.RepositoryInterfaces.UserRepository;
 import WebService.ServiceImplementation.UserServiceImplmentation;
+import WebService.Shared.dto.AddressDTO;
 import WebService.Shared.dto.UserDto;
+import WebService.Shared.dto.Utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 public class UserServiceImplTest {
@@ -25,9 +31,23 @@ public class UserServiceImplTest {
     @Mock
     UserRepository userRepository;
 
+    String userId="uabsuabsains12131";
+    @Mock
+    Utils utils;
+    UserEntity userEntity;
+    String enrcyptedpass="9u3923923u23";
+    @Mock
+    BCryptPasswordEncoder bCryptPasswordEncoder;
     @BeforeEach
     void setUp() throws Exception{
         MockitoAnnotations.initMocks(this);
+        //this methode is called first
+        userEntity=new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setFirstName("Sergey");
+        userEntity.setUserId(userId);
+        userEntity.setEncryptedPassword(enrcyptedpass);
+
     }
 
     //MOKITO FRAMEWORK IS USED TO MOCK THOSE CLASSES THAT OUR TEST FUNCTION NEEDS
@@ -35,11 +55,7 @@ public class UserServiceImplTest {
     @Test
     final void testGetUser(){
         //STUB IS A HARDCODED FAKE OBJECT CONTAINING HARDCODED VALUE TESTING PURPOSES
-        UserEntity userEntity=new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setFirstName("Sergey");
-        userEntity.setUserId("uabsuabsains12131");
-        userEntity.setEncryptedPassword("9u3923923u23");
+
 
         //created a mock and stubbed it with a stub object
         //part of
@@ -68,5 +84,38 @@ public class UserServiceImplTest {
 
     }
 
+    // we write unit test for a method.
+    // for writing junit test cases for a method find out all the objects that you might need to mock
+
+
+    @Test
+    final void testCreateUser(){
+    //mocking the first dependent object
+
+
+        when(userRepository.findByEmail( anyString() )).thenReturn(null);
+
+        when(utils.generatedAddressId(anyInt())).thenReturn("dasadeasasa");
+
+        when(utils.generatedUserId(anyInt())).thenReturn(userId);
+
+        when(bCryptPasswordEncoder.encode(anyString())).thenReturn(enrcyptedpass);
+
+        when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
+
+        AddressDTO addressDTO=new AddressDTO();
+        addressDTO.setType("Shipping");
+        List<AddressDTO> addressDTOList=new ArrayList<>();
+        addressDTOList.add(addressDTO);
+
+        UserDto userDto=new UserDto();
+        userDto.setAddresses(addressDTOList);
+
+        UserDto stored=userServiceImplmentation.createUser(userDto);
+        assertNotNull(stored);
+        assertEquals(userEntity.getFirstName(),stored.getFirstName());
+
+
+    }
 }
 
