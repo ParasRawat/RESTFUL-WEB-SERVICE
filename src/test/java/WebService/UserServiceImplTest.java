c9d625eafdc7684fec460aef6ca5f6eb9aec9349
Paras Rawat <parasmyname@gmail.com>
2020-03-03 19:59:26 +0530

    More testing, Verifying that methodes are called certain number of times

--- src/test/java/WebService/UserServiceImplTest.java
+++ src/test/java/WebService/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package WebService;
 
+import WebService.Entity.AddressEntity;
 import WebService.Entity.UserEntity;
 import WebService.RepositoryInterfaces.UserRepository;
 import WebService.ServiceImplementation.UserServiceImplmentation;
@@ -11,15 +12,18 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class UserServiceImplTest {
 
@@ -47,6 +51,7 @@ public class UserServiceImplTest {
         userEntity.setFirstName("Sergey");
         userEntity.setUserId(userId);
         userEntity.setEncryptedPassword(enrcyptedpass);
+        userEntity.setAddresses(getAddressEntity());
 
     }
 
@@ -93,6 +98,7 @@ public class UserServiceImplTest {
     //mocking the first dependent object
 
 
+        //filling your methodes with stubs for execution
         when(userRepository.findByEmail( anyString() )).thenReturn(null);
 
         when(utils.generatedAddressId(anyInt())).thenReturn("dasadeasasa");
@@ -103,19 +109,54 @@ public class UserServiceImplTest {
 
         when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
 
-        AddressDTO addressDTO=new AddressDTO();
-        addressDTO.setType("Shipping");
-        List<AddressDTO> addressDTOList=new ArrayList<>();
-        addressDTOList.add(addressDTO);
+
 
         UserDto userDto=new UserDto();
-        userDto.setAddresses(addressDTOList);
+        userDto.setFirstName("Paras");
+        userDto.setLastName("Rawat");
+        userDto.setPassword("cnsidsn32");
+        userDto.setEmail("test@gmail.com");
+        userDto.setAddresses(getAddressDto());
 
         UserDto stored=userServiceImplmentation.createUser(userDto);
         assertNotNull(stored);
         assertEquals(userEntity.getFirstName(),stored.getFirstName());
+        assertEquals(userEntity.getLastName(),stored.getLastName());
+        assertNotNull(stored.getUserId());
+        assertEquals(stored.getAddresses().size(),userEntity.getAddresses().size());
 
+        //testing that the functions are called only desired number of times
+        verify(utils,times(2)).generatedAddressId(30);
+        verify(bCryptPasswordEncoder,times(1)).encode("cnsidsn32");
+        verify(userRepository,times(1)).save(any(UserEntity.class));
+    }
 
+    private List<AddressDTO> getAddressDto(){
+        AddressDTO addressDTO=new AddressDTO();
+        addressDTO.setType("Shipping");
+        addressDTO.setCity("Hisar");
+        addressDTO.setCountry("India");
+        addressDTO.setPostalCode("ABDS1");
+        addressDTO.setStreetName("57- A Tower Enclave");
+        List<AddressDTO> addressDTOList=new ArrayList<>();
+        addressDTOList.add(addressDTO);
+
+        AddressDTO billaddressDTO=new AddressDTO();
+        billaddressDTO.setType("Billing");
+        billaddressDTO.setCity("Hisar");
+        billaddressDTO.setCountry("India");
+        billaddressDTO.setPostalCode("ABDS1");
+        billaddressDTO.setStreetName("57- A Tower Enclave");
+
+        addressDTOList.add(billaddressDTO);
+        return addressDTOList;
+    }
+    private List<AddressEntity> getAddressEntity(){
+
+        List<AddressDTO> addressDTOS=getAddressDto();
+        Type listType =new TypeToken<List<AddressEntity>>(){}.getType();
+
+        return new ModelMapper().map(addressDTOS,listType);
     }
 }
 
